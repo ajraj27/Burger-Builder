14401002178988f57dc0009d29f1dfc85ed1eaaf
ajraj27 <rajanuj.iiita@gmail.com>
2019-01-10 01:13:25 +0530

    Displayed burger

--- src/components/Burger/Ingredient/Ingredient.js
+++ src/components/Burger/Ingredient/Ingredient.js
@@ -15,7 +15,7 @@ class BurgerIngredient extends Component {
 
       case ("bread-top"):
           ingredient=(
-                <div className={classes.BreadBottom}>
+                <div className={classes.BreadTop}>
                       <div className={classes.Seeds1}></div>
                       <div className={classes.Seeds2}></div>
                 </div>
@@ -40,7 +40,7 @@ class BurgerIngredient extends Component {
 
       default: ingredient=null;
     }
-    
+
       return ingredient;
   }
 }
@@ -49,4 +49,4 @@ BurgerIngredient.propTypes={
   type:PropTypes.string.isRequired
 }
 
-default export burgIngredient;
+ export default BurgerIngredient;
