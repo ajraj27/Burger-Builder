7a9e5c2e14978879627d3eba1a7ad3b4895529dc
ajraj27 <rajanuj.iiita@gmail.com>
2019-01-11 03:21:50 +0530

    Dynamically added burger components

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,20 @@ import BurgerIngredient from './Ingredient/Ingredient';
 import classes from './Burger.css';
 
 const burger=(props) => {
+  const transformedIngredients=Object.keys(props.ingredients)
+  .map(igkey => {
+    return [...Array(props.ingredients[igkey])].map((_,i) => {
+            return <BurgerIngredient key={igkey+i} type={igkey} />
+    });
+  });
+
+  console.log(transformedIngredients);
 
   return (
         <div className={classes.Burger}>
-            <BurgerIngredient type='bread-top'/>
-            <BurgerIngredient type='cheese'/>
-            <BurgerIngredient type='meat'/>
-            <BurgerIngredient type='bread-bottom'/>
+          <BurgerIngredient type='bread-top'/>
+            {transformedIngredients}
+        <BurgerIngredient type='bread-bottom'/>
         </div>
   )
 }
