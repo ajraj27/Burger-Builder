8bdbef4c9ff53c1f1256be568cff88aea6cc1733
ajraj27 <rajanuj.iiita@gmail.com>
2019-01-15 13:12:47 +0530

    Added a button component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary=(props) => {
   const listIngredients=Object.keys(props.ingredients)
