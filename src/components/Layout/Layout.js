323811ed0b5d8aecd24f0f1f87d4c7efeea1a886
ajraj27 <rajanuj.iiita@gmail.com>
2019-01-24 02:06:27 +0530

    Enabled toggling of sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,11 +6,19 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import Sidedrawer from '../Navigation/Sidedrawer/Sidedrawer';
 
 class Layout extends Component{
+  state={
+    showSideDrawer:true
+  }
+
+  closeSideDrawerHandler=() => {
+    this.setState({showSideDrawer:false});
+  }
+
   render(){
     return (
       <Aux>
           <Toolbar />
-          <Sidedrawer />
+          <Sidedrawer open={this.state.showSideDrawer} close={this.closeSideDrawerHandler}/>
           <main className={classes.Content}>
               {this.props.children}
           </main>
--- src/components/Navigation/Sidedrawer/Sidedrawer.js
+++ src/components/Navigation/Sidedrawer/Sidedrawer.js
@@ -3,18 +3,29 @@ import React from 'react';
 import classes from './Sidedrawer.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sidedrawer=(props) => {
-  return(
-      <div className={classes.Sidedrawer}>
-      <div className={classes.Logo}>
-          <Logo />
-      </div>
-          <nav>
-              <NavigationItems />
-          </nav>
+  let attachedClassses=[classes.Sidedrawer,classes.Close];
 
-      </div>
+  if(props.open){
+    attachedClassses=[classes.Sidedrawer,classes.Open];
+  }
+
+  return(
+      <Aux>
+        <Backdrop show={props.open} clicked={props.close} />
+        <div className={attachedClassses.join(' ')}>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+            <nav>
+                <NavigationItems />
+            </nav>
+
+        </div>
+      </Aux>
   )
 }
 
