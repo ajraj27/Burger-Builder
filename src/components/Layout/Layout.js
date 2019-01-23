62cdf0218d930cb22709b27882c9c698158fabd5
ajraj27 <rajanuj.iiita@gmail.com>
2019-01-23 23:54:10 +0530

    Chenged layout to stateful component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React,{Component} from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import Sidedrawer from '../Navigation/Sidedrawer/Sidedrawer';
 
-const layout=(props) => (
-  <Aux>
-      <Toolbar />
-      <Sidedrawer />
-      <main className={classes.Content}>
-          {props.children}
-      </main>
-  </Aux>
-)
+class Layout extends Component{
+  render(){
+    return (
+      <Aux>
+          <Toolbar />
+          <Sidedrawer />
+          <main className={classes.Content}>
+              {props.children}
+          </main>
+      </Aux>
+    )
+  }
+}
 
-export default layout;
+export default Layout;
