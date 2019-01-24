a038b4abba6c6e0ecf6ee9f656bf48048e52b181
ajraj27 <rajanuj.iiita@gmail.com>
2019-01-24 11:07:14 +0530

    Addded toggle button

--- src/components/Navigation/Sidedrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/Sidedrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import classes from './DrawerToggle.css';
+
 const drawerToggle=(props) => (
-    <div onClick={props.toggleDrawer}>Menu</div>
+    <div className={classes.DrawerToggle} onClick={props.toggleDrawer}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 );
 
 export default drawerToggle;
