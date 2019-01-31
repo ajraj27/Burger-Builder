1b38f88e1675cbd14e50a58fded6c26cedf8c3ff
ajraj27 <rajanuj.iiita@gmail.com>
2019-01-31 21:26:13 +0530

    Showed error modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import WithErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICE={
   salad:0.3,
@@ -147,4 +148,4 @@ class BurgerBuilder extends Component{
   }
 }
 
-export default BurgerBuilder;
+export default WithErrorHandler(BurgerBuilder,axios);
